1586b968db48ff5a9857c41791f33d128838398b
Deepak11k11 <121069079+Deepak11k11@users.noreply.github.com>
2025-03-16 23:02:02 +0530

    updated to show all posts in the database

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,63 +1,5 @@
-// const Post = require("../models/post.model");
-// const cloudinary = require("../config/cloudinary");
-// const fs = require("fs");
-
-
-// const createPost = async (req, res) => {
-//     try {
-//         if (!req.file) return res.status(400).json({ message: "No file uploaded" });
-
-//         // Upload file to Cloudinary
-//         const result = await cloudinary.uploader.upload(req.file.path, {
-//             resource_type: "auto",
-//             folder: "social_media_posts",
-//         });
-
-//         // Delete the local file after upload
-//         fs.unlink(req.file.path, (err) => {
-//             if (err) {
-//                 console.error("Error deleting local file:", err);
-//             } else {
-//                 console.log("Local file deleted successfully");
-//             }
-//         });
-
-//         // Save post to database
-//         const post = new Post({
-//             user: req.user.id,
-//             caption: req.body.caption,
-//             mediaUrl: result.secure_url,
-//             mediaType: result.resource_type === "image" ? "image" : "video",
-//         });
-
-//         await post.save();
-//         res.status(201).json({ message: "Post created successfully", post });
-//     } catch (error) {
-//         res.status(500).json({ message: "Server Error", error });
-//     }
-// };
-
-// module.exports = { createPost };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const Post = require("../models/post.model");
-const User = require("../models/users.model"); // Make sure the path is correct
+const User = require("../models/users.model");
 const Like = require("../models/like.model");
 const Comment = require("../models/comment.model");
 const cloudinary = require("../config/cloudinary");
@@ -104,19 +46,13 @@ const createPost = async (req, res) => {
     }
 };
 
-// @desc    Get posts for users the current user follows, with likes count and comments
+// @desc    Get all posts from the database with likes count and comments
 // @route   POST /api/posts/all
 // @access  Private
 const getPosts = async (req, res) => {
     try {
-        // Find the current user to get the following list
-        const currentUser = await User.findById(req.user.id);
-        if (!currentUser) {
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        // Find posts from users that the current user follows
-        const posts = await Post.find({ user: { $in: currentUser.following } })
+        // Find all posts, sorted by most recent, and populate the user details
+        const posts = await Post.find({})
             .sort({ createdAt: -1 })
             .populate("user", "name username");
 
